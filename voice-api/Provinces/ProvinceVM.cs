cd861c4008505fc0ff8374f7e10736e82bfe5147
DESKTOP-QRRVCHG\ADMIN <ducdat010102@gmail.com>
2022-10-03 16:38:06 +0700

    create province api bu duc dat

---
+++ voice-api/Provinces/ProvinceVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace VoiceAPI.Provinces
+{
+    public class ProvinceVM
+    {
+        public string name { get; set; }
+        public string code { get; set; }
+        public Guid id { get; internal set; }
+    }
+    public class ProvicesVM : ProvinceVM
+    {
+        public int id { get; set; }
+    }
+}
