2e75e8c446d8f74a511b9ae1a7a61a70d4720e09
trungndse150553 <trungndse150553@fpt.edu.vn>
2022-10-04 12:04:15 +0700

    Category API by Duc Trung

---
+++ voice-api/Categories/CategoryVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace VoiceAPI.Categories
+{
+    public class CategoryVM
+    {
+        public string Name { get; set; }
+        public Guid Id { get; set; }
+    }
+
+    public class Category : CategoryVM
+    {
+        //public Guid Id { get; set; }
+    }
+}
---
+++ voice-api/Controller/CategoryController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using VoiceAPI.Categories;
+
+namespace VoiceAPI.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        public static List<CategoryVM> categories = new List<CategoryVM>();
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(categories);
+        }
+
+        [HttpGet ("{id}")]
+        public IActionResult GetById(string id)
+        {
+            try
+            {
+                var category = categories.SingleOrDefault(ct => ct.Id == Guid.Parse(id));
+                if (category == null)
+                {
+                    return NotFound();
+                }
+                return Ok(category);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Create(CategoryVM categoryVM)
+        {
+            var category = new CategoryVM
+            {
+                Id = Guid.NewGuid(),
+                Name = categoryVM.Name
+            };
+            categories.Add(category);
+            return Ok(new
+            {
+                Success = true,
+                Data = category
+            });
+        }
+
+        [HttpPut ("{id}")]
+        public IActionResult Edit(string id, CategoryVM categoryEdit)
+        {
+            try
+            {
+                var category = categories.SingleOrDefault(ct => ct.Id == Guid.Parse(id));
+                if (category == null)
+                {
+                    return NotFound();
+                }
+                
+                if (id != category.Id.ToString())
+                {
+                    return BadRequest();
+                }
+                category.Name = categoryEdit.Name;
+                return Ok();
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
