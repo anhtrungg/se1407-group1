35579d506de2f3870b67b5a30ea489567063d32f
trungndse150553 <trungndse150553@fpt.edu.vn>
2022-10-07 22:33:18 +0700

    Review table API and Fix Category API

---
+++ voice-api/Controllers/CategoryController.cs
@@ -0,0 +1,104 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using VoiceAPI.Entities;
+using VoiceAPI.Helpers;
+using VoiceAPI.IServices;
+using VoiceAPI.Models.Common;
+using VoiceAPI.Models.Payload.Categories;
+using VoiceAPI.Models.Responses.Categories;
+
+namespace VoiceAPI.Controllers
+{
+    [Route("api/v{version:apiVersion}/category")]
+    [ApiController]
+    [ApiVersion("1")]
+    public class CategoryController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly ITools _tools;
+
+        private readonly ICategoryService _categoryService;
+        public CategoryController(IMapper mapper, ITools tools, ICategoryService categoryService)
+        {
+            _mapper = mapper;
+            _tools = tools;
+            _categoryService = categoryService;
+        }
+
+        [HttpGet("{id}")]
+        [MapToApiVersion("1")]
+        [Authorize(Roles = "ADMIN")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var result = await _categoryService.GetById(id);
+            if (result.IsSuccess)
+                return Ok(new SingleObjectResponse<CategoryDetailDTO>(result.Data));
+            Response.StatusCode = result.StatusCode;
+            var response = _mapper.Map<ErrorResponse>(result);
+            return StatusCode(result.StatusCode, response);
+        }
+
+        [HttpPost]
+        [MapToApiVersion("1")]
+        public async Task<IActionResult> CreateNew(CategoryCreatePayload payloads)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var result = await _categoryService.CreateNew(payloads);
+            if (result.IsSuccess)
+                return StatusCode((int)HttpStatusCode.Created,
+                    new SingleObjectResponse<CategoryDTO>(result.Data));
+            Response.StatusCode = result.StatusCode;
+            var response = _mapper.Map<ErrorResponse>(result);
+            return StatusCode(result.StatusCode, response);
+        }
+
+        [HttpGet]
+        [MapToApiVersion("1")]
+        [Authorize(Roles = "ADMIN")]
+        public async Task<IActionResult> GetAllWithSubCategories()
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var result = await _categoryService.GetAllWithSubCategories();
+            if (result.IsSuccess)
+                return Ok(new MultiObjectResponse<CategoryDetailDTO>(result.Data));
+            Response.StatusCode = result.StatusCode;
+            var response = _mapper.Map<ErrorResponse>(result);
+            return StatusCode(result.StatusCode, response);
+        }
+
+        //[HttpPut]
+        //[MapToApiVersion("1")]
+        //public async Task<IActionResult> UpdateCategory(CategoryCreatePayload payloads)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
+
+        //    var result = await _categoryService.UpdateCategory(payloads);
+        //    if (result.IsSuccess)
+        //        return StatusCode((int)HttpStatusCode.Created,
+        //            new SingleObjectResponse<CategoryDetailDTO>(result.Data));
+        //    Response.StatusCode = result.StatusCode;
+        //    var response = _mapper.Map<ErrorResponse>(result);
+        //    return StatusCode(result.StatusCode, response);
+        //}
+    }
+}
---
+++ voice-api/Controllers/ReviewController.cs
@@ -0,0 +1,54 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Threading.Tasks;
+using VoiceAPI.Helpers;
+using VoiceAPI.IServices;
+using VoiceAPI.Models.Common;
+using VoiceAPI.Models.Data.Reviews;
+using VoiceAPI.Models.Payload.Categories;
+using VoiceAPI.Models.Payload.Reviews;
+using VoiceAPI.Models.Responses.Categories;
+using VoiceAPI.Models.Responses.Reviews;
+using VoiceAPI.Services;
+
+namespace VoiceAPI.Controllers
+{
+    [Route("api/v{version:apiVersion}/review")]
+    [ApiController]
+    [ApiVersion("1")]
+    public class ReviewController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly ITools _tools;
+
+        private readonly IReviewService _reviewService;
+        public ReviewController(IMapper mapper, ITools tools, IReviewService reviewService)
+        {
+            _mapper = mapper;
+            _tools = tools;
+            _reviewService = reviewService;
+        }
+
+        [HttpPost]
+        [MapToApiVersion("1")]
+        [Authorize(Roles = "ENTERPRISE")]
+        public async Task<IActionResult> EnterpriseCreateReview(ReviewEnterpriseCreateDataModel dataModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var result = await _reviewService.EnterpriseCreateReview(dataModel);
+            if (result.IsSuccess)
+                return StatusCode((int)HttpStatusCode.Created,
+                    new SingleObjectResponse<ReviewDTO>(result.Data));
+            Response.StatusCode = result.StatusCode;
+            var response = _mapper.Map<ErrorResponse>(result);
+            return StatusCode(result.StatusCode, response);
+        }
+    }
+}
